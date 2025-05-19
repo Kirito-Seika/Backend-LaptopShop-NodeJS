eac9fbba8826c3f1392bb60c842d20903ad2a999
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-05-19 11:02:42 +0700

    Hello World with Express

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello Edan Nguyen");
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on link: http://localhost:${port}`);
+})
