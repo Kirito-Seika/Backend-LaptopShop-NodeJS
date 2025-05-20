dde64ef0746630123b4b219631f3f874e2f653d3
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-05-21 04:42:41 +0700

    Setup view engine

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 8000;
+const hostname = process.env.HOST_NAME;
+
+// config view engine
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home.ejs');
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on link: http://localhost:${port}`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 })
