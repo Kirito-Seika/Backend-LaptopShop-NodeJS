04de540cc9ef31db8ae0690b2f3b39c93801467d
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-05-21 11:37:38 +0700

    Setup MySQL with NodeJS

---
+++ src/config/database.ts
@@ -0,0 +1,24 @@
+import mysql from 'mysql2/promise';
+
+const Connection = async () => {
+    const connection = await mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'laptopshop',
+    });
+
+    try {
+        const [results, fields] = await connection.query(
+            'SELECT * FROM `users`'
+        );
+
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export default Connection;
